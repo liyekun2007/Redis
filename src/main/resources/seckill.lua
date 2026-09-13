6cb651aa6508d991aaf40d9054fbb9b80e767570
itcast <hello@itcast.cn>
2026-09-13 16:04:44 +0800

    Redis优化秒杀

---
+++ src/main/resources/seckill.lua
@@ -0,0 +1,25 @@
+--1.参数列表
+local voucherId =ARGV[1]
+local userId =ARGV[2]
+
+--2.数据key
+local stockKey ='seckkill:stock' .. voucherId
+local orderKey ='seckkill:order' .. voucherId
+
+--3.脚本业务
+if (tonumber(redis.call('get' ,stockKey) <= 0)) then
+     --库存不足
+    return 1
+end
+
+--判断用户是否下单
+if(reids.call('sismember',orderKey,userId)==1) then
+    --存在说明是重复下单，返回2
+    return 2
+end
+
+--扣库存
+redis.call('incrby',stockKey,-1)
+--下单（保存用户）
+reids.call('sadd',orderKey,userId)
+return 0
