90558bc50cdc5df24c0cc73ce7136c6055a81550
itcast <hello@itcast.cn>
2026-09-16 17:58:41 +0800

    查询探店信息（完善版）

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisConstants;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -16,5 +24,30 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
+    @Resource
+    private IUserService userService;
+    @Resource
+    private CacheClient cacheClient;
 
+    @Override
+    public Result queryBlogById(Long id) {
+        // 使用 CacheClient 逻辑过期方案查询
+        Blog blog =getById(id);
+        if(blog ==null){
+            return Result.fail("笔记不存在!");
+        }
+        queryBlogUser(blog);
+        return Result.ok(blog);
+    }
+
+
+
+
+
+    private void queryBlogUser(Blog blog) {
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
+    }
 }
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,18 +1,42 @@
 package com.hmdp;
 
+import com.hmdp.entity.Blog;
+import com.hmdp.service.IBlogService;
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisConstants;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
     @Resource
     private ShopServiceImpl shopService;
+    @Resource
+    private CacheClient cacheClient;
+    @Resource
+    private IBlogService blogService;
+
     @Test
     void testSaveShop(){
         shopService.saveShop2Redis(1L,10L);
+    }
+
+    @Test
+    void testSaveBlogCache(){
+        // 1. 查询所有博客
+        List<Blog> blogs = blogService.list();
+        // 2. 逐个写入 Redis（逻辑过期 30 分钟）
+        for (Blog blog : blogs) {
+            String key = RedisConstants.CACHE_BLOG_KEY + blog.getId();
+            cacheClient.setWithLogicalExpire(key, blog, 30L, TimeUnit.MINUTES);
+            System.out.println("缓存博客 id=" + blog.getId());
+        }
+        System.out.println("博客缓存预热完成，共 " + blogs.size() + " 条");
     }
 }
